5a20c4ae3458f5194e456c950f02e73220451e2e
Jason Ernst <ernstjason1@gmail.com>
2024-08-12 10:04:57 -0700

    fix minsdk issue

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.github.compscidr.icmp_android"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
